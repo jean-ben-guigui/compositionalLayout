20ea6540de11e823cee482fe8f00bf63c65e052a
jean-ben-guigui <duver.arthur@gmail.com>
2020-05-07 14:53:43 +0200

    Initial Commit

---
+++ galleryPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  galleryPhoto
+//
+//  Created by Arthur Duver on 07/05/2020.
+//  Copyright © 2020 Arthur Duver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
