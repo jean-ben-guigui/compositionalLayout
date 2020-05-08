3a04a27ef9ab4abd14bba83ac0f5402845309b6c
jean-ben-guigui <duver.arthur@gmail.com>
2020-05-08 19:32:37 +0200

    end part3

---
+++ galleryPhoto/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  galleryPhoto
+//
+//  Created by Arthur Duver on 07/05/2020.
+//  Copyright © 2020 Arthur Duver. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let identifier = "collectionCellReuseIdentifier"
+	@IBOutlet weak var title: UILabel!
+}
--- galleryPhoto/ViewController.swift
+++ galleryPhoto/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+	//MARK: - properties
+	enum Section {
+        case main
+    }
+
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+	
+	//MARK: - Outlets
+	@IBOutlet weak var collectionView: UICollectionView!
+	
+	//MARK: - Functions
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		configureDataSource()
 	}
+	
+	private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
 
+            // Get a cell of the desired kind.
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: CollectionViewCell.identifier,
+                for: indexPath) as? CollectionViewCell else { fatalError("Cannot create new cell") }
 
+            // Populate the cell with our item description.
+            cell.title.text = "\(identifier)"
+
+            // Return the cell.
+            return cell
+        }
+
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<94))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
 }
 
